3cad71ce6bc5887f4d3a91c5d467d66a693488fb
Unknown <mikk633k@hotmail.com>
2019-02-27 11:26:34 +0100

    Basic setup with pages and components

---
+++ h2-app/src/Pages/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
