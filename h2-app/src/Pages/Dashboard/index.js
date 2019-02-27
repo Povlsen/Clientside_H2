953d592c5c4b4ce85758057d46cd0fe6d0631be0
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-27 12:29:34 +0100

    Dashboard css added

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import './dashboard.css';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="Dashboard">
-        <header className="App-header">
-          <p>Dashboard</p>
-        </header>
+      <div className="dashboard_grid">
+        <div> Opret Ansatte </div>
+        <div> Juster Løn </div>
+        <div> Alle Ansatte </div>
+        <div> Afdelinger </div>
       </div>
     );
   }
