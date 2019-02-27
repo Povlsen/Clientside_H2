67b66944638c7aca83181214721e5b6e7083238f
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-27 14:09:41 +0100

    Added text

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
   render() {
     return (
-      <body className="dashboard_grid">
-        <div className="grid_item"> Opret Ansatte </div>
-        <div className="grid_item"> Juster Løn </div>
-        <div className="grid_item"> Alle Ansatte </div>
-        <div className="grid_item"> Afdelinger </div>
+      <body className="dashboard_grid">        
+        <div className="grid_item"> <h3> Opret Ansatte </h3> </div>
+        <div className="grid_item"> <h3> Juster Løn </h3></div>
+        <div className="grid_item"> <h3> Alle Ansatte </h3></div>
+        <div className="grid_item"> <h3> Afdelinger </h3> </div>
       </body>
     );
   }
