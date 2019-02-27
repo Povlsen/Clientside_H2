1c087d03e4bc4e6848421e1098da1abe3ccde9c0
Emil Neirup <emilnjensen@gmail.com>
2019-02-27 12:46:58 +0100

    Added navbar

--- h2-app/src/App.js
+++ h2-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Dashboard from './Pages/Dashboard';
+import Navbar from './Components/navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Dashboard />
+        
       </div>
     );
   }
