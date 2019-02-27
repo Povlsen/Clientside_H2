bd68fd605da979e79ea1e73d286ef604fccf5931
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-27 13:04:48 +0100

    Dashboard edited

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -4,12 +4,12 @@ import './dashboard.css';
 class Dashboard extends Component {
   render() {
     return (
-      <ol className="dashboard_grid">
+      <body className="dashboard_grid">
         <div className="grid_item"> Opret Ansatte </div>
         <div className="grid_item"> Juster Løn </div>
         <div className="grid_item"> Alle Ansatte </div>
         <div className="grid_item"> Afdelinger </div>
-      </ol>
+      </body>
     );
   }
 }
