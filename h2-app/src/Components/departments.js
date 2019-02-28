0572b4be8817fc0137fc2ac5249e9b66f0bbbaff
Emil Neirup <emilnjensen@gmail.com>
2019-02-28 08:47:39 +0100

    created new components

---
+++ h2-app/src/Components/departments.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Departments extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+        <header className="App-header">
+          <h2>
+                Departments;
+          </h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Departments;
