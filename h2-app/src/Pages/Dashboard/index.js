76bcef06d8aaadb3880c9887d7eeac2a4b4a32e1
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-28 11:23:52 +0100

    SVG ICONS for cards
    Doesn´t load

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -6,19 +6,19 @@ class Dashboard extends Component {
       <body className="dashboard_grid">        
         <div className="grid_item"> 
           <h3>Opret Ansatte</h3> 
-          <img/>
+          <img srcSet="../../../res/icon_add_user.svg" alt=""/>
         </div>
         <div className="grid_item"> 
           <h3>Juster Løn</h3>
-          <img/>
+          <img srcSet="../../../res/icon_salary.svg" alt=""/>
         </div>
         <div className="grid_item"> 
           <h3>Alle Ansatte</h3>
-          <img/>
+          <img srcSet="../../../res/icon_users.svg" alt=""/>
         </div>
         <div className="grid_item"> 
           <h3>Afdelinger</h3> 
-          <img/>
+          <img srcSet="../../../res/icon_departments.svg" alt="" />
         </div>
       </body>
     );
