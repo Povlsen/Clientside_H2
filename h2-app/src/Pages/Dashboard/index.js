2e0938096df74b6f3cad3c26ac8ecabd60718930
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-28 14:19:13 +0100

    Cards fixed

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -5,19 +5,19 @@ class Dashboard extends Component {
     return (
       <body className="dashboard_grid">        
         <div className="grid_item"> 
-          <h3>Opret Ansatte</h3> 
+          <h3>Add employee</h3> 
           <img src="https://image.flaticon.com/icons/svg/39/39632.svg" alt=""/>
         </div>
         <div className="grid_item"> 
-          <h3>Juster Løn</h3>
+          <h3>Salary</h3>
           <img src="https://image.flaticon.com/icons/svg/150/150430.svg" alt=""/>
         </div>
         <div className="grid_item"> 
-          <h3>Alle Ansatte</h3>
+          <h3>Employees</h3>
           <img src="https://image.flaticon.com/icons/svg/33/33308.svg" alt=""/>
         </div>
         <div className="grid_item"> 
-          <h3>Afdelinger</h3> 
+          <h3>Departments</h3> 
           <img src="https://image.flaticon.com/icons/svg/50/50616.svg" alt="" />
         </div>
       </body>
