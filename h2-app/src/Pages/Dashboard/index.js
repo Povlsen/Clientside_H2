3b187ad103f9567b132086a713c435403ac040d5
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-02-28 08:48:02 +0100

    Cards in dashboard update

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -4,10 +4,22 @@ class Dashboard extends Component {
   render() {
     return (
       <body className="dashboard_grid">        
-        <div className="grid_item"> <h3> Opret Ansatte </h3> </div>
-        <div className="grid_item"> <h3> Juster Løn </h3></div>
-        <div className="grid_item"> <h3> Alle Ansatte </h3></div>
-        <div className="grid_item"> <h3> Afdelinger </h3> </div>
+        <div className="grid_item"> 
+          <h3>Opret Ansatte</h3> 
+          <img/>
+        </div>
+        <div className="grid_item"> 
+          <h3>Juster Løn</h3>
+          <img/>
+        </div>
+        <div className="grid_item"> 
+          <h3>Alle Ansatte</h3>
+          <img/>
+        </div>
+        <div className="grid_item"> 
+          <h3>Afdelinger</h3> 
+          <img/>
+        </div>
       </body>
     );
   }
