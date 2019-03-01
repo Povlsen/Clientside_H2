80e788f9b53c95c9dfa8c176e35a0631ddf2aa7c
Emil Neirup <emilnjensen@gmail.com>
2019-03-01 11:23:31 +0100

    Modify merge conflict

--- h2-app/src/Pages/Adjust Employee/index.js
+++ h2-app/src/Pages/Adjust Employee/index.js
@@ -16,17 +16,9 @@ class Adjust_Employee extends Component {
 
     render() {
       return (
-<<<<<<< HEAD
-        <ul class="list-group">
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-      )
-=======
+
         <h3> {this.state.employee.Id} </h3>
       );
->>>>>>> 8089d7e0ba4f0b9c672997b836889c2dc512b503
     }
   };
       
