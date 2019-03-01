d7c6a947dbb7f9c27ffc680b0f36b566903e2376
Unknown <mikk633k@hotmail.com>
2019-03-02 00:31:16 +0100

    Styling finishes on dashboard and redirection

--- h2-app/src/Pages/Dashboard/index.js
+++ h2-app/src/Pages/Dashboard/index.js
@@ -3,22 +3,23 @@ import './index.scss';
 
 class Dashboard extends Component {
   render() {
+    console.log('dash', this.props)
     return (
       <body className="dashboard_grid">        
-        <div className="grid_item"> 
-            <h3 class="hover hover-1">Add employee</h3> 
+        <div className="grid_item" onClick={() => this.props.history.push(`/employees/0`)}> 
+            <h3 className="theamAnimation">Add employee</h3> 
             <img src="https://image.flaticon.com/icons/svg/39/39632.svg" alt=""/>
         </div>
-        <div className="grid_item"> 
-          <h3 class="hover hover-2">Salary</h3>
+        <div className="grid_item inactive"> 
+          <h3 className="theamAnimation">Salary</h3>
           <img src="https://image.flaticon.com/icons/svg/150/150430.svg" alt=""/>
         </div>
-        <div className="grid_item"> 
-          <h3 class="hover hover-3">Employees</h3>
+        <div className="grid_item" onClick={() => this.props.history.push(`/employees`)}> 
+          <h3 className="theamAnimation">Employees</h3>
           <img src="https://image.flaticon.com/icons/svg/33/33308.svg" alt=""/>
         </div>
-        <div className="grid_item"> 
-          <h3 class="hover hover-4">Departments</h3> 
+        <div className="grid_item" onClick={() => this.props.history.push(`/departments`)}> 
+          <h3 className="theamAnimation">Departments</h3> 
           <img src="https://image.flaticon.com/icons/svg/50/50616.svg" alt="" />
         </div>
       </body>
