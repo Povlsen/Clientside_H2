bafb9731219758e7e856f16a733192fae12ab7da
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2019-03-01 11:09:26 +0100

    Working edit employee SELECT

--- h2-app/src/Pages/Adjust Employee/index.js
+++ h2-app/src/Pages/Adjust Employee/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './index.scss';
+import { getEmployee, postEmployee } from '../../Utils/Employees';
+
 
 class Adjust_Employee extends Component {
+  state = {
+    employee: {}
+  }
+
+  componentDidMount() {
+    getEmployee(this.props.match.params.Id).then(res => {
+      this.setState({ employee: res })
+    }).catch(err => console.log(err)) //TODO: better error handeling
+  }
+
     render() {
-      return (<div/>
-      )
+      return (
+        <h3> {this.state.employee.Id} </h3>
+      );
     }
   };
       
