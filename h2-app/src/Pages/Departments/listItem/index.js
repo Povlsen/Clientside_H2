41d7c667ae85729be91f8f543a3c9b7033871c56
Emil Neirup <emilnjensen@gmail.com>
2019-03-02 13:54:19 +0100

    Forberedelse til Department list - Installeret mdbootstrap

---
+++ h2-app/src/Pages/Departments/listItem/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { getDateString } from '../../../Utils/helpers'
+import './index.scss'
+
+class Item extends Component {
+  state = {
+    employee: {
+      Id: 0,
+      birthDate: null,
+      firstName: "",
+      lastName: "",
+      gender: "",
+      hireDate: null
+    }
+  }
+
+  componentDidMount() {
+    var item = this.props.item
+    this.setState({
+      employee: {
+        Id: item.Id,
+        birthDate: getDateString(item.birthDate),
+        hireDate: getDateString(item.hireDate),
+        firstName: item.firstName,
+        lastName: item.lastName,
+        gender: item.gender
+      }
+    })
+  }
+
+
+  render() {
+    var item = this.state.employee
+    return (
+      <div className="EmployeeItem" onClick={this.props.onClick.bind(this, item.Id)}>
+        <div className="list_item_id">{item.Id}</div>
+        <div>{item.firstName}</div>
+        <div>{item.lastName}</div>
+        <div>{item.birthDate}</div>
+        <div>{item.gender}</div>
+        <div className="list_item_hire">{item.hireDate}</div>
+      </div>
+    )
+  }
+}
+
+export default Item
